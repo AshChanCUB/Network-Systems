5e80a5739f939b39d4a308cfae4aa1cca00aa7e2
Ashwin Chandrasekaran <143350160+AshChanCUB@users.noreply.github.com>
2023-09-26 10:43:20 -0600

    Update udp_client.c

--- Programming Assignment - 1/udp/udp_client.c
+++ Programming Assignment - 1/udp/udp_client.c
@@ -8,19 +8,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
- 
-
 #define BUFSIZE 1024
 #define MAXFILENAME 256
-
- 
 
 void error(char *msg) {
     perror(msg);
     exit(1);
 }
-
- 
 
 int main(int argc, char *argv[]) {
     int sockfd;
@@ -32,30 +26,23 @@ int main(int argc, char *argv[]) {
     char buffer[BUFSIZE];
     int n;
 
- 
-
     if (argc != 3) {
-        fprintf(stderr, "usage: %s <hostname> <port>\n", argv[0]);
-        exit(0);
+        fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
+        exit(1);
     }
+
     hostname = argv[1];
     portno = atoi(argv[2]);
-
- 
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
 
- 
-
     server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host as %s\n", hostname);
-        exit(0);
+        exit(1);
     }
-
- 
 
     bzero((char *)&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
@@ -63,24 +50,26 @@ int main(int argc, char *argv[]) {
     serveraddr.sin_port = htons(portno);
     serverlen = sizeof(serveraddr);
 
- 
-
     while (1) {
         // Get user command
         printf("Enter command: ");
         bzero(buffer, BUFSIZE);
         fgets(buffer, BUFSIZE, stdin);
 
- 
-       if (strcmp(buffer, "ls") == 0) {
+        if (strcmp(buffer, "ls\n") == 0) {
             // Handle the "ls" command
             char response[BUFSIZE];
             bzero(response, BUFSIZE);
-            
+
+            // Send the "ls" command to the server
+            n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
+            if (n < 0)
+                error("ERROR sending command to server");
+
             // Receive the server's response (the list of files)
             while (1) {
                 bzero(buffer, BUFSIZE);
-                n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr*)&serveraddr, &serverlen);
+                n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
                 if (n <= 0) {
                     break;
                 }
@@ -89,12 +78,14 @@ int main(int argc, char *argv[]) {
                 }
                 strcat(response, buffer);
             }
-            
+
             // Print the list of files received from the server
             printf("List of files on the server:\n%s", response);
-        }else if (strcmp(buffer, "exit\n") == 0) {
+        } else if (strcmp(buffer, "exit\n") == 0) {
             // Handle the "exit" command
-            sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
+            n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
+            if (n < 0)
+                error("ERROR sending command to server");
             close(sockfd);
             printf("Client is exiting.\n");
             exit(0);
@@ -108,7 +99,6 @@ int main(int argc, char *argv[]) {
             if (n < 0)
                 error("ERROR sending command to server");
 
- 
             // Receive and save the file from the server
             FILE *received_file = fopen(filename, "wb");
             if (received_file == NULL) {
@@ -133,8 +123,6 @@ int main(int argc, char *argv[]) {
                 error("ERROR sending command to server");
         }
     }
-
- 
 
     close(sockfd);
     return 0;
