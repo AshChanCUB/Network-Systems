d4e7c309287517d059c90b262d4af2386ba3ea91
Ashwin Chandrasekaran <143350160+AshChanCUB@users.noreply.github.com>
2023-09-26 11:23:08 -0600

    Update udp_client.c

--- Programming Assignment - 1/udp/udp_client.c
+++ Programming Assignment - 1/udp/udp_client.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h> // Include the pthread library
 
 #define BUFSIZE 1024
 #define MAXFILENAME 256
@@ -14,6 +15,33 @@
 void error(char *msg) {
     perror(msg);
     exit(1);
+}
+
+// Function to receive a file from the server
+void receiveFile(int sockfd, struct sockaddr_in serveraddr, socklen_t serverlen, char *filename) {
+    FILE *received_file = fopen(filename, "wb");
+    if (received_file == NULL) {
+        perror("Error opening file for writing");
+        return;
+    }
+
+    char buffer[BUFSIZE];
+    int n;
+
+    while (1) {
+        bzero(buffer, BUFSIZE);
+        n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
+        if (n <= 0) {
+            break;
+        }
+        if (strcmp(buffer, "END\n") == 0) {
+            break; // End of file transfer
+        }
+        fwrite(buffer, 1, n, received_file);
+    }
+
+    fclose(received_file);
+    printf("Received file: %s\n", filename);
 }
 
 int main(int argc, char *argv[]) {
@@ -56,26 +84,7 @@ int main(int argc, char *argv[]) {
         bzero(buffer, BUFSIZE);
         fgets(buffer, BUFSIZE, stdin);
 
-        if (strcmp(buffer, "ls\n") == 0) {
-            // Handle the "ls" command
-            char response[BUFSIZE];
-            bzero(response, BUFSIZE);
-
-            // Send the "ls" command to the server
-            n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
-            if (n < 0)
-                error("ERROR sending command to server");
-
-            // Receive and print the server's response (the list of files)
-            while (1) {
-                bzero(buffer, BUFSIZE);
-                n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
-                if (n <= 0) {
-                    break;
-                }
-                printf("%s", buffer);
-            }
-        } else if (strcmp(buffer, "exit\n") == 0) {
+        if (strcmp(buffer, "exit\n") == 0) {
             // Handle the "exit" command
             n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
             if (n < 0)
@@ -93,28 +102,30 @@ int main(int argc, char *argv[]) {
             if (n < 0)
                 error("ERROR sending command to server");
 
-            // Receive and save the file from the server
-            FILE *received_file = fopen(filename, "wb");
-            if (received_file == NULL) {
-                perror("Error opening file for writing");
-            } else {
-                // Receive and write the file data from the server
-                while (1) {
-                    bzero(buffer, BUFSIZE);
-                    n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
-                    if (n <= 0) {
-                        break;
-                    }
-                    fwrite(buffer, 1, n, received_file);
-                }
-                fclose(received_file);
-                printf("Received file: %s\n", filename);
-            }
+            // Create a thread to receive the file while the main loop continues
+            pthread_t thread;
+            pthread_create(&thread, NULL, (void *(*)(void *))receiveFile, (void *) (intptr_t) sockfd);
+            pthread_detach(thread); // Detach the thread to avoid resource leak
+
+            // Note: The main loop will continue to execute and accept user input
         } else {
             // Send other commands to the server
             n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
             if (n < 0)
                 error("ERROR sending command to server");
+
+            // Handle responses from the server
+            while (1) {
+                bzero(buffer, BUFSIZE);
+                n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
+                if (n <= 0) {
+                    break;
+                }
+                if (strcmp(buffer, "END\n") == 0) {
+                    break; // End of response
+                }
+                printf("%s", buffer);
+            }
         }
     }
 
