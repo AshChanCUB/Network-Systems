0ab583fba368a9f870bd95235af9e6240d252ed1
Ashwin Chandrasekaran <143350160+AshChanCUB@users.noreply.github.com>
2023-09-26 11:01:43 -0600

    Update udp_client.c

--- Programming Assignment - 1/udp/udp_client.c
+++ Programming Assignment - 1/udp/udp_client.c
@@ -7,13 +7,43 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h> // Added for errno
+#include <stdbool.h> // Added for boolean type
 
 #define BUFSIZE 1024
 #define MAXFILENAME 256
+#define TIMEOUT_SECONDS 10 // Adjust the timeout as needed
 
 void error(char *msg) {
     perror(msg);
     exit(1);
+}
+
+// Function to handle receiving data with a timeout
+bool receiveWithTimeout(int sockfd, struct sockaddr_in *serveraddr, socklen_t *serverlen, char *buffer, int bufsize, int timeoutSec) {
+    fd_set readfds;
+    struct timeval timeout;
+    FD_ZERO(&readfds);
+    FD_SET(sockfd, &readfds);
+    timeout.tv_sec = timeoutSec;
+    timeout.tv_usec = 0;
+
+    int ready = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
+    if (ready < 0) {
+        perror("Error in select");
+        return false;
+    } else if (ready == 0) {
+        printf("Timeout: No response received from the server.\n");
+        return false;
+    } else {
+        // Data is available to be received
+        int n = recvfrom(sockfd, buffer, bufsize, 0, (struct sockaddr *)serveraddr, serverlen);
+        if (n <= 0) {
+            perror("Error receiving data from server");
+            return false;
+        }
+        return true;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -66,21 +96,14 @@ int main(int argc, char *argv[]) {
             if (n < 0)
                 error("ERROR sending command to server");
 
-            // Receive the server's response (the list of files)
-            while (1) {
-                bzero(buffer, BUFSIZE);
-                n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
-                if (n <= 0) {
-                    break;
+            // Receive the server's response (the list of files) with a timeout
+            if (receiveWithTimeout(sockfd, &serveraddr, &serverlen, response, BUFSIZE, TIMEOUT_SECONDS)) {
+                if (strcmp(response, "END\n") == 0) {
+                    printf("List of files on the server:\n");
+                } else {
+                    printf("List of files on the server:\n%s", response);
                 }
-                if (strcmp(buffer, "END\n") == 0) {
-                    break; // End of response
-                }
-                strcat(response, buffer);
             }
-
-            // Print the list of files received from the server
-            printf("List of files on the server:\n%s", response);
         } else if (strcmp(buffer, "exit\n") == 0) {
             // Handle the "exit" command
             n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serveraddr, serverlen);
@@ -99,22 +122,26 @@ int main(int argc, char *argv[]) {
             if (n < 0)
                 error("ERROR sending command to server");
 
-            // Receive and save the file from the server
+            // Receive and save the file from the server with a timeout
             FILE *received_file = fopen(filename, "wb");
             if (received_file == NULL) {
                 perror("Error opening file for writing");
             } else {
-                // Receive and write the file data from the server
+                bool receiveSuccess = false;
                 while (1) {
                     bzero(buffer, BUFSIZE);
-                    n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
-                    if (n <= 0) {
+                    receiveSuccess = receiveWithTimeout(sockfd, &serveraddr, &serverlen, buffer, BUFSIZE, TIMEOUT_SECONDS);
+                    if (!receiveSuccess) {
                         break;
                     }
                     fwrite(buffer, 1, n, received_file);
                 }
-                fclose(received_file);
-                printf("Received file: %s\n", filename);
+                if (receiveSuccess) {
+                    fclose(received_file);
+                    printf("Received file: %s\n", filename);
+                } else {
+                    printf("Failed to receive the file: %s\n", filename);
+                }
             }
         } else {
             // Send other commands to the server
