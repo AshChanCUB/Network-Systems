4bd5755f0fcf67ebfb6c2a5a78e22150603aed2d
Ashwin Chandrasekaran <143350160+AshChanCUB@users.noreply.github.com>
2023-10-03 01:58:16 -0400

    Update udp_server.c

--- Programming Assignment - 1/udp/udp_server.c
+++ Programming Assignment - 1/udp/udp_server.c
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <dirent.h>
+#include <fcntl.h>
 
 #define BUFSIZE 1024
 #define MAXFILENAME 256
@@ -17,11 +18,11 @@ void error(char *msg) {
     exit(1);
 }
 
-void sendFile(int sockfd, char *filename) {
+void sendFile(int sockfd, struct sockaddr_in clientaddr, socklen_t clientlen, char *filename) {
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         char error_msg[] = "File not found.";
-        send(sockfd, error_msg, strlen(error_msg), 0);
+        sendto(sockfd, error_msg, strlen(error_msg), 0, (struct sockaddr *)&clientaddr, clientlen);
         return;
     }
 
@@ -29,7 +30,7 @@ void sendFile(int sockfd, char *filename) {
     ssize_t bytes_read;
 
     while ((bytes_read = fread(buffer, 1, BUFSIZE, file)) > 0) {
-        ssize_t bytes_sent = send(sockfd, buffer, bytes_read, 0);
+        ssize_t bytes_sent = sendto(sockfd, buffer, bytes_read, 0, (struct sockaddr *)&clientaddr, clientlen);
         if (bytes_sent < 0) {
             perror("Error sending file data");
             break;
@@ -37,38 +38,13 @@ void sendFile(int sockfd, char *filename) {
     }
 
     fclose(file);
+
+    // Send the "END\n" marker to indicate the end of file transfer
     char end_marker[] = "END\n";
-    send(sockfd, end_marker, strlen(end_marker), 0);
+    sendto(sockfd, end_marker, strlen(end_marker), 0, (struct sockaddr *)&clientaddr, clientlen);
 }
 
-void receiveFile(int sockfd, char *filename) {
-    char buffer[BUFSIZE];
-    FILE *file = fopen(filename, "wb");
-    if (file == NULL) {
-        char error_msg[] = "Error opening file for writing.";
-        send(sockfd, error_msg, strlen(error_msg), 0);
-        return;
-    }
-
-    while (1) {
-        bzero(buffer, BUFSIZE);
-        ssize_t bytes_received = recv(sockfd, buffer, BUFSIZE, 0);
-        if (bytes_received <= 0) {
-            break;
-        }
-
-        if (strcmp(buffer, "END\n") == 0) {
-            break;
-        }
-
-        fwrite(buffer, 1, bytes_received, file);
-    }
-
-    fclose(file);
-    printf("Received file: %s\n", filename);
-}
-
-void listFiles(int sockfd) {
+void listFiles(int sockfd, struct sockaddr_in clientaddr, socklen_t clientlen) {
     DIR *dir;
     struct dirent *ent;
     char file_list[BUFSIZE] = "";
@@ -82,16 +58,21 @@ void listFiles(int sockfd) {
         }
         closedir(dir);
 
-        send(sockfd, file_list, strlen(file_list), 0);
+        ssize_t n = sendto(sockfd, file_list, strlen(file_list), 0, (struct sockaddr *)&clientaddr, clientlen);
+        if (n < 0) {
+            error("ERROR in sendto");
+        }
     } else {
         char response[] = "Error opening directory.\n";
-        send(sockfd, response, strlen(response), 0);
+        sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
     }
 }
 
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd;
+    int sockfd;
     int portno;
+    struct sockaddr_in serveraddr;
+    struct sockaddr_in clientaddr;
     socklen_t clientlen;
     char buffer[BUFSIZE];
     int optval = 1;
@@ -103,13 +84,12 @@ int main(int argc, char *argv[]) {
     }
     portno = atoi(argv[1]);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
 
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 
-    struct sockaddr_in serveraddr, clientaddr;
     bzero((char *)&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -118,59 +98,98 @@ int main(int argc, char *argv[]) {
     if (bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
         error("ERROR on binding");
 
-    listen(sockfd, 5);
     clientlen = sizeof(clientaddr);
 
     while (1) {
-        newsockfd = accept(sockfd, (struct sockaddr *)&clientaddr, &clientlen);
-        if (newsockfd < 0)
-            error("ERROR on accept");
+        bzero(buffer, BUFSIZE);
+        n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&clientaddr, &clientlen);
+        if (n < 0)
+            error("ERROR in recvfrom");
 
-        pid_t pid = fork();
-        if (pid == 0) {
-            close(sockfd);
-            while (1) {
-                bzero(buffer, BUFSIZE);
-                n = read(newsockfd, buffer, BUFSIZE);
-                if (n < 0)
-                    error("ERROR reading from socket");
-
-                if (strcmp(buffer, "exit\n") == 0) {
-                    printf("Client is exiting.\n");
-                    break;
-                } else if (strncmp(buffer, "get ", 4) == 0) {
-                    char filename[MAXFILENAME];
-                    sscanf(buffer, "get %s", filename);
-                    sendFile(newsockfd, filename);
-                } else if (strncmp(buffer, "put ", 4) == 0) {
-                    char filename[MAXFILENAME];
-                    sscanf(buffer, "put %s", filename);
-                    receiveFile(newsockfd, filename);
-                } else if (strncmp(buffer, "delete ", 7) == 0) {
-                    char filename[MAXFILENAME];
-                    sscanf(buffer, "delete %s", filename);
-                    if (remove(filename) == 0) {
-                        printf("Deleted file: %s\n", filename);
-                        char response[] = "File deleted successfully.\n";
-                        send(newsockfd, response, strlen(response), 0);
-                    } else {
-                        perror("Error deleting file");
-                        char response[] = "Error deleting file.\n";
-                        send(newsockfd, response, strlen(response), 0);
-                    }
-                } else if (strcmp(buffer, "ls\n") == 0) {
-                    listFiles(newsockfd);
-                } else {
-                    char response[] = "Unknown command or incorrect input.\n";
-                    send(newsockfd, response, strlen(response), 0);
-                }
-            }
-            close(newsockfd);
-            exit(0);
-        } else if (pid < 0) {
-            error("ERROR on fork");
+        // Remove trailing newline character if present
+        if (buffer[n - 1] == '\n') {
+            buffer[n - 1] = '\0';
+            n--;
         }
-        close(newsockfd);
+
+        if (strncmp(buffer, "get ", 4) == 0) {
+            char filename[MAXFILENAME];
+            sscanf(buffer, "get %s", filename);
+
+            if (access(filename, F_OK) != -1) {
+                sendFile(sockfd, clientaddr, clientlen, filename);
+                printf("File transfer successful: %s\n", filename); // Print success message
+            } else {
+                char response[BUFSIZE];
+                snprintf(response, BUFSIZE, "File not found: %s", filename);
+                sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
+            }
+
+            // Send "END" marker to indicate the end of response
+            char end_marker[] = "END\n";
+            sendto(sockfd, end_marker, strlen(end_marker), 0, (struct sockaddr *)&clientaddr, clientlen);
+
+        } else if (strncmp(buffer, "put ", 4) == 0) {
+            char filename[MAXFILENAME];
+            sscanf(buffer, "put %s", filename);
+
+            FILE *file = fopen(filename, "wb");
+            if (file == NULL) {
+                perror("Error opening file");
+            } else {
+                while (1) {
+                    bzero(buffer, BUFSIZE);
+                    n = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&clientaddr, &clientlen);
+                    if (n <= 0) {
+                        break;
+                    }
+                    fwrite(buffer, 1, n, file);
+                }
+                fclose(file);
+                printf("File transfer successful: %s\n", filename); // Print success message
+            }
+
+            // Send "END" marker to indicate the end of response
+            char end_marker[] = "END\n";
+            sendto(sockfd, end_marker, strlen(end_marker), 0, (struct sockaddr *)&clientaddr, clientlen);
+
+        } else if (strcmp(buffer, "ls") == 0) {
+            listFiles(sockfd, clientaddr, clientlen);
+
+            // Send "END" marker to indicate the end of response
+            char end_marker[] = "END\n";
+            sendto(sockfd, end_marker, strlen(end_marker), 0, (struct sockaddr *)&clientaddr, clientlen);
+
+        } else if (strncmp(buffer, "delete ", 7) == 0) {
+            char filename[BUFSIZE];
+            sscanf(buffer, "delete %s", filename);
+            if (remove(filename) == 0) {
+                printf("Deleted file: %s\n", filename);
+                char response[] = "File deleted successfully.\n";
+                sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
+            } else {
+                perror("Error deleting file");
+                char response[] = "Error deleting file.\n";
+                sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
+            }
+
+            // Send "END" marker to indicate the end of response
+            char end_marker[] = "END\n";
+            sendto(sockfd, end_marker, strlen(end_marker), 0, (struct sockaddr *)&clientaddr, clientlen);
+
+        } else if (strcmp(buffer, "exit") == 0) {
+            printf("Server is exiting gracefully.\n");
+            close(sockfd);
+            exit(0);
+        } else {
+            char response[BUFSIZE];
+            snprintf(response, BUFSIZE, "Unknown command: %s\n", buffer);
+            sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
+
+            // Send "END" marker to indicate the end of response
+            char end_marker[] = "END\n";
+            sendto(sockfd, end_marker, strlen(end_marker), 0, (struct sockaddr *)&clientaddr, clientlen);
+        }
     }
 
     close(sockfd);
